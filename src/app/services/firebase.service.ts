2324a21693c0c8350d751785cb39c9d88027ff10
Elias Mora <eliamora94@gmail.com>
2017-09-12 16:55:26 -0400

    Ya sube imagenes

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,20 +1,44 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class FirebaseService {
   productos:FirebaseListObservable<any[]>;
   producto:FirebaseObjectObservable<any[]>;
-  constructor(private af: AngularFireDatabase) { }
+  folder:any;
+
+  constructor(private af: AngularFireDatabase) {
+    this.folder = 'productoimages';
+   }
 
   getProductos(){
     this.productos = this.af.list('/productos/') as FirebaseListObservable<Producto>
-    return this.productos
+    return this.productos;
   }
 
   getProductoDetails(id){
     this.producto = this.af.object('/productos/'+id) as FirebaseObjectObservable<Producto>
-    return this.producto
+    return this.producto;
+  }
+
+  addProducto(producto){
+    console.log("Paso1");
+    let storageRef = firebase.storage().ref();
+    console.log("Paso2");
+    for(let selectedFile of [(<HTMLInputElement>document.getElementById('productofoto')).files[0]]){
+      console.log(selectedFile.name);
+      console.log("Paso3");
+      let path=`/${this.folder}/${selectedFile.name}`;
+      let iRef = storageRef.child(path);
+      console.log("Paso4");
+      iRef.put(selectedFile).then((snapshot) => {
+        producto.foto = selectedFile.name;
+        producto.path = path;
+        return this.af.database.ref('productos/'+producto.id).set(producto);    
+        //return this.productos.push(producto);
+      });
+    }
   }
 }
 
