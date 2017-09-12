d438602e89e605369789dd650d2a9c005e0b81be
Elias Mora <eliamora94@gmail.com>
2017-09-12 09:56:35 -0400

    import 'rxjs/add/operator/switchMap';

--- src/app/producto-search.component.ts
+++ src/app/producto-search.component.ts
@@ -11,6 +11,7 @@ import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
 
 import { ProductoSearchService } from './producto-search.service';
 import { Producto } from './producto';
