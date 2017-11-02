e905882c51f6015df71a0a85286c3fff77b53883
santiago <santiagoarcep@gmail.com>
2017-11-02 19:20:14 -0300

    grafico 1

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -12,57 +12,24 @@ export class AdminComponent implements OnInit {
   echarts = Chart;
   constructor(private firebaseService:FirebaseService) { }
   ventas:any;
+  cantidad_productos:number[]=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  nombres_productos:string[]=[""];
+  list_item:any[]=[""];
 
   ngOnInit() {
     this.firebaseService.getVentas().subscribe(ventas => {
         this.ventas = ventas;
         console.log(ventas);
+        this.funcionSantiago();
+        this.showCharts1();
     });
 
-    var Chart1 = this.echarts.init(document.getElementById('grafico1'));
+    
     var Chart2 = this.echarts.init(document.getElementById('grafico2'));
     var Chart3 = this.echarts.init(document.getElementById('grafico3'));
     var Chart4 = this.echarts.init(document.getElementById('grafico4'));
-    
-    Chart1.setOption({
-        title : {
-            text: 'Productos Vendidos',
-            subtext: '(cantidad)',
-            x:'center'
-        },
-        tooltip : {
-            trigger: 'item',
-            formatter: "{a} <br/>{b} : {c} ({d}%)"
-        },
-        legend: {
-            orient: 'vertical',
-            left: 'left',
-            data: ['data1','data2','data3','data4','data5']
-        },
-        series : [
-            {
-                name: 'cantidad',
-                type: 'pie',
-                radius : '55%',
-                center: ['50%', '60%'],
-                data:[
-                    {value:335, name:'data1'},
-                    {value:310, name:'data2'},
-                    {value:234, name:'data3'},
-                    {value:135, name:'data4'},
-                    {value:1548, name:'data5'}
-                ],
-                itemStyle: {
-                    emphasis: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
-                    }
-                }
-            }
-        ]   
-    });
 
+    
     Chart2.setOption({
         title: {
             text: 'Cantidad Acumulada',
@@ -268,5 +235,101 @@ export class AdminComponent implements OnInit {
     });
 
   }
+  funcionSantiago(){
+    
+    var i=0;
+    var band=0;
+    this.ventas.forEach(venta=>{
+        venta.items.forEach(item=>{
+            band=0;
+            this.nombres_productos.forEach(nombre=>{
+                if (item.producto == nombre)
+                {
+                    band=1;
+                }
 
+            })
+            if (band == 0)
+            {
+                this.nombres_productos.push(item.producto);
+            }
+        })
+    });
+    console.log("nombres de los productos")
+    console.log(this.nombres_productos)//se obtiene los nombres de los productos
+
+    ///////////////////////////////////////
+    this.ventas.forEach(venta=>{
+        venta.items.forEach(item=>{
+            this.list_item.push(item)
+        })
+            })
+    console.log(this.list_item)
+    //////////////////////////////////////
+    var i=0;
+    this.nombres_productos.forEach(nombre=>{
+        this.list_item.forEach(item=>{
+            if (nombre==item.producto)
+            {
+                this.cantidad_productos[i]=this.cantidad_productos[i]+item.cantidad;
+            }
+        })
+        i++;
+    })
+    for (var _i = 0; _i < this.cantidad_productos.length-1; _i++)
+    {
+        var aux=this.cantidad_productos[_i];
+        this.cantidad_productos[_i]=this.cantidad_productos[_i+1];
+        this.cantidad_productos[_i+1]=aux;
+        var aux2=this.nombres_productos[_i];
+        this.nombres_productos[_i]=this.nombres_productos[_i+1];
+        this.nombres_productos[_i+1]=aux2;
+    }
+    console.log("Resultado:")
+    console.log(this.nombres_productos);
+    console.log(this.cantidad_productos);
+    }
+
+    showCharts1(){
+        const Chart1 = this.echarts.init(document.getElementById('grafico1'));
+        Chart1.setOption({
+            title : {
+                text: 'Productos Vendidos',
+                subtext: '(cantidad)',
+                x:'center'
+            },
+            tooltip : {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left',
+                data: [this.nombres_productos[0],this.nombres_productos[1],this.nombres_productos[2],this.nombres_productos[3],this.nombres_productos[4]]
+            },
+            series : [
+                {
+                    name: 'Cantidad Vendida',
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['50%', '60%'],
+                    data:[
+                        {value:this.cantidad_productos[0], name:this.nombres_productos[0]},
+                        {value:this.cantidad_productos[1], name:this.nombres_productos[1]},
+                        {value:this.cantidad_productos[2], name:this.nombres_productos[2]},
+                        {value:this.cantidad_productos[3], name:this.nombres_productos[3]},
+                        {value:this.cantidad_productos[4], name:this.nombres_productos[4]}
+                    ],
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]   
+        });
+
+    }
 }
