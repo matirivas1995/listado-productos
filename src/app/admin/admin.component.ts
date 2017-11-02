c031d78070d444b418eaae651f017d7244d3f602
Elias Mora <eliamora94@gmail.com>
2017-11-02 10:52:00 -0300

    charts

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Chart from 'echarts';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
+  echarts = Chart;
   constructor() { }
 
   ngOnInit() {
+    var myChart = this.echarts.init(document.getElementById('main'));
+    myChart.setOption({
+      title: { text: 'ECharts entry example' },
+      tooltip: {},
+      xAxis: {
+          data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+      },
+      yAxis: {},
+      series: [{
+          name: 'sales',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+      }]
+  });
   }
 
 }
