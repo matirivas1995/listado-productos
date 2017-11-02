6601e61ef43f015199732531e5ffe27747123bad
santiago <santiagoarcep@gmail.com>
2017-11-02 11:17:50 -0300

    grafico 2

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -56,17 +56,51 @@ export class AdminComponent implements OnInit {
     });
 
     Chart2.setOption({
-      title: { text: 'ECharts entry example' },
-      tooltip: {},
-      xAxis: {
-          data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
-      },
-      yAxis: {},
-      series: [{
-          name: 'sales',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-      }]
+        title: {
+            text: '对数轴示例',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c}'
+        },
+        legend: {
+            left: 'left',
+            data: ['2的指数', '3的指数']
+        },
+        xAxis: {
+            type: 'category',
+            name: 'x',
+            splitLine: {show: false},
+            data: ['一', '二', '三', '四', '五', '六', '七', '八', '九']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        yAxis: {
+            type: 'log',
+            name: 'y'
+        },
+        series: [
+            {
+                name: '3的指数',
+                type: 'line',
+                data: [1, 3, 9, 27, 81, 247, 741, 2223, 6669]
+            },
+            {
+                name: '2的指数',
+                type: 'line',
+                data: [1, 2, 4, 8, 16, 32, 64, 128, 256]
+            },
+            {
+                name: '1/2的指数',
+                type: 'line',
+                data: [1/2, 1/4, 1/8, 1/16, 1/32, 1/64, 1/128, 1/256, 1/512]
+            }
+        ]
     });
 
     Chart3.setOption({
