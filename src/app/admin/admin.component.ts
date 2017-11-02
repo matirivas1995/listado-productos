7510570f3f7f5839dd4b9554e949e5ffa869883e
Elias Mora <eliamora94@gmail.com>
2017-11-02 11:09:39 -0300

    4 charts

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -11,8 +11,12 @@ export class AdminComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    var myChart = this.echarts.init(document.getElementById('main'));
-    myChart.setOption({
+    var Chart1 = this.echarts.init(document.getElementById('grafico1'));
+    var Chart2 = this.echarts.init(document.getElementById('grafico2'));
+    var Chart3 = this.echarts.init(document.getElementById('grafico3'));
+    var Chart4 = this.echarts.init(document.getElementById('grafico4'));
+    
+    Chart1.setOption({
       title: { text: 'ECharts entry example' },
       tooltip: {},
       xAxis: {
@@ -24,7 +28,50 @@ export class AdminComponent implements OnInit {
           type: 'bar',
           data: [5, 20, 36, 10, 10, 20]
       }]
-  });
+    });
+
+    Chart2.setOption({
+      title: { text: 'ECharts entry example' },
+      tooltip: {},
+      xAxis: {
+          data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+      },
+      yAxis: {},
+      series: [{
+          name: 'sales',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+      }]
+    });
+
+    Chart3.setOption({
+      title: { text: 'ECharts entry example' },
+      tooltip: {},
+      xAxis: {
+          data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+      },
+      yAxis: {},
+      series: [{
+          name: 'sales',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+      }]
+    });
+
+    Chart4.setOption({
+      title: { text: 'ECharts entry example' },
+      tooltip: {},
+      xAxis: {
+          data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+      },
+      yAxis: {},
+      series: [{
+          name: 'sales',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+      }]
+    });
+
   }
 
 }
